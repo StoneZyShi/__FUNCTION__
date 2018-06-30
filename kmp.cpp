6fc4e32d563d75446393630c02c912a0ccb7490f
StoneZyShi <2297455360@qq.com>
2018-06-30 12:18:16 +0800

    creat kmp.cpp

---
+++ kmp.cpp
@@ -0,0 +1,67 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* P 为模式串，下标从 0 开始 */
+void GetNext(string P, int next[])
+{
+	int p_len = (int)(P.size());
+	int i = 0;   // P 的下标
+	int j = -1;
+	next[0] = -1;
+
+	while (i < p_len)
+	{
+		if (j == -1 || P[i] == P[j])
+		{
+			i++;
+			j++;
+			next[i] = j;
+		}
+		else
+			j = next[j];
+	}
+}
+
+/* 在 S 中找到 P 第一次出现的位置 */
+int KMP(string S, string P, int next[])
+{
+	GetNext(P, next);
+	for (int a = 0; a < 100; a++)
+		cout << next[a] << " ";
+	cout << endl;
+
+	int i = 0;  // S 的下标
+	int j = 0;  // P 的下标
+	int s_len = (int)(S.size());
+	int p_len = (int)(P.size());
+
+	while (i < s_len && j < p_len)
+	{
+		if (j == -1 || S[i] == P[j])  // P 的第一个字符不匹配或 S[i] == P[j]
+		{
+			i++;
+			j++;
+		}
+		else
+			j = next[j];  // 当前字符匹配失败，进行跳转
+		cout << i << "   " << j << endl;
+	}
+
+	if (j == p_len)  // 匹配成功
+		return i - j;
+
+	return -1;
+}
+
+int main()
+{
+	int next[100] = { 0 };
+
+	cout << KMP("bbc abcdab abcdabcdabde", "abcdabd", next) << endl; // 15
+
+	return 0;
+}
+
